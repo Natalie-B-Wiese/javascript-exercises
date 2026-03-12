37abf45bdaa0273e4b0fa236711b89650d773de2
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-12 11:34:25 -0400

    Complete tempConversion exercise
    All tests pass and the functions for converting from celcius to fahrenheit and vice versa work

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // (fahrenheit - 32) * (5/9) = celcius
+  let celcius=(fahrenheit-32) * (5/9);
+
+  //round answer to 1 decimal
+  celcius=(Math.round(celcius*10)) / 10;
+
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  // (celcius * (9/5)) + 32 = fahrenheit
+  let fahrenheit=(celcius*(9/5)) + 32;
+
+  //round answer to 1 decimal
+  fahrenheit=(Math.round(fahrenheit*10)) / 10;
+
+
+  return fahrenheit;
+
 };
 
 // Do not edit below this line
