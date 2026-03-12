2d70ae1ff90fddf7ca8477a46deec0c10da43604
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-12 11:20:31 -0400

    Complete sumAll exercise
    All tests pass

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    if (num1<0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+    {
+        return "ERROR";
+    }
+    else
+    {
+        let smallNum;
+        let bigNum;
+
+        if (num1<num2)
+        {
+            smallNum=num1;
+            bigNum=num2;
+        }
+        else
+        {
+            smallNum=num2;
+            bigNum=num1;
+        }
+
+        let sum=0;
+        for (i=smallNum; i<=bigNum; i++)
+        {
+            sum+=i;
+        }
+        return sum;
+
+    }
+
+    
+
 
 };
 
