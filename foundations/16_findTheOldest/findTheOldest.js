44084063fd4481ed48d7d6fed9b4672ed127b926
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-13 14:32:29 -0400

    Complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    /*
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    */
+   //if year of death is blank, then use today's year
 
+   array.sort(function(a,b){
+    const thisYear=new Date().getFullYear();
+
+    const aYears=a.yearOfBirth - (a.yearOfDeath ? a.yearOfDeath : thisYear);
+    const bYears=b.yearOfBirth - (b.yearOfDeath ? b.yearOfDeath : thisYear);
+    if (aYears>bYears)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+   });
+
+   return array[0];
+   
 };
 
 // Do not edit below this line
