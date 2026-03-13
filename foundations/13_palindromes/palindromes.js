d86e3c4a4b7246e9542d20b616ab9fdbe7ca2773
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-13 13:52:22 -0400

    Complete palindromes exercise
    All tests on palindromes pass

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    //Removes punctuation and spaces from the string and makes it case insensitive
+    stringStripped = (string.replace(/[^a-zA-Z0-9]/g, '')).toLowerCase();
+    const backward=stringStripped.split('').toReversed().join('');
+
+    return stringStripped===backward;
 
 };
 
