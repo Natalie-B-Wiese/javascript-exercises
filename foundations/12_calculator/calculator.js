66e3f311147823be6791741b220cf461392a3863
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-13 13:38:43 -0400

    Complete calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accumulator, currentValue)=>accumulator+currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue)=>accumulator*currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer=1;
+  for (let i=1; i<=num; i++)
+  {
+    answer*=i;
+  }
+  return answer;	
 };
 
 // Do not edit below this line
