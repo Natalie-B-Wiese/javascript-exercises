92633463009243a8402c7af696a084ec4ce5e5e9
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-13 14:05:50 -0400

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num<0)
+    {
+        return "OOPS";
+    }
+    if (num==0)
+    {
+        return 0;
+    }
+
+    let arr=[];
+    let sum=1;
+
+    for (let i=0; i<num; i++)
+    {
+        arr[i]=sum;
+        if(arr.length>2)
+        {
+            sum=arr[i-1]+arr[i];
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
